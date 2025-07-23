0f4a1c4c69aa8c07487c7c4910400df8d489fc89
yanislammari <yanis.lammari937@gmail.com>
2025-07-23 16:49:49 +0200

    Add navigation

---
+++ src/components/constants/MenuItems.tsx
@@ -0,0 +1,14 @@
+import MenuItem from "../../models/structs/MenuItem";
+
+const menuItems: MenuItem[] = [
+  { path: '/', label: 'Tableau de bord' },
+  { path: '/espaces', label: 'Espaces' },
+  { path: '/animaux', label: 'Animaux' },
+  { path: '/employes', label: 'Employés' },
+  { path: '/billets', label: 'Billets' },
+  { path: '/settings', label: 'Paramètres' },
+  { path: '/settings', label: 'Paramètre2s' }
+
+];
+
+export default menuItems;
